91e7418f646505cbe40762d4e187f8e2e240e9b3
Jose Hugo De la cruz Romero <jhugodc@gmail.com>
2019-10-08 18:21:16 -0500

    print log

--- index.js
+++ index.js
@@ -12,8 +12,10 @@ const publicKeyBuf = utils.toBuffer(keyPair.publicKey)
 
 const address = utils.pubToAddress(publicKeyBuf, true)
 
-console.log('--------------------')
-console.log('Sender address: ', utils.bufferToHex(address))
+//console.log('--------------------')
+//console.log('Sender address: ', utils.bufferToHex(address))
+
+var calling = false
 
 const account = new Account({
   balance: 100e18,
@@ -24,6 +26,9 @@ vm.stateManager.putAccount(address, account, async function() {
   const tx2 = require('./raw-tx2')
 
   await runTx(vm, tx1, privateKey)
+
+  calling = true
+  
   await runTx(vm, tx2, privateKey)
   
 })
@@ -32,9 +37,9 @@ async function runTx(vm, rawTx, pk) {
   const tx = new Transaction(rawTx)
   tx.sign(pk)
 
-  console.log('tx: ', tx)
+  //console.log('tx: ', tx)
 
-  console.log('------ running tx -------')
+  //console.log('------ running tx -------')
   const results = await vm.runTx({
     tx: tx,
   })
@@ -47,9 +52,19 @@ async function runTx(vm, rawTx, pk) {
   }
   
   console.log('---')
+
+  console.log('gasUsed: ', results.gasUsed)
+  console.log('returnValue: ', results.execResult.returnValue)
 }
 
 vm.on('step', function(data) {
-  console.log(`Opcode: ${data.opcode.name}\tStack: ${data.stack}`)
+  if (calling && data.opcode.name != 'MUL' && data.opcode.name != 'DUP3') {
+    console.log('-----------------------------------------------------------------------------------------------------------')
+    console.log(`${data.pc} ${data.opcode.name}\n`)
+    console.log('STACK')
+    for (var i=0; i < data.stack.length; i++)
+      console.log(`${i} - ${data.stack[i]}`)
+  }
 })
+
 
